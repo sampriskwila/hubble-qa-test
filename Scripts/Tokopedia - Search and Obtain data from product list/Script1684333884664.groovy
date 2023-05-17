9b15bf1ae1551dfc1aa0a0b5051250f650e22f00
sam_priskwila <sam.pris77@gmail.com>
2023-05-17 21:43:14 +0700

    change page load timeout to 30 seconds

--- Scripts/Tokopedia - Search and Obtain data from product list/Script1684333884664.groovy
+++ Scripts/Tokopedia - Search and Obtain data from product list/Script1684333884664.groovy
@@ -1,25 +1,23 @@
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-
-import org.openqa.selenium.Keys
-import org.openqa.selenium.WebElement
-
+import org.openqa.selenium.Keys as Keys
+import org.openqa.selenium.WebElement as WebElement
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 
 WebUI.openBrowser('')
 
 WebUI.navigateToUrl('https://www.tokopedia.com/')
 
-WebUI.waitForPageLoad(10)
+WebUI.waitForPageLoad(30)
 
 WebUI.setText(findTestObject('input_search'), 'iphone 14 pro')
 
 WebUI.sendKeys(findTestObject('input_search'), Keys.chord(Keys.ENTER))
 
-WebUI.waitForPageLoad(10)
+WebUI.waitForPageLoad(30)
 
 WebUI.click(findTestObject('official_store_checkbox'))
 
-WebUI.waitForPageLoad(10)
+WebUI.waitForPageLoad(30)
 
 WebUI.scrollToPosition(0, 500)
 
@@ -27,7 +25,7 @@ WebUI.setText(findTestObject('input_price_min'), '100000')
 
 WebUI.sendKeys(findTestObject('input_price_min'), Keys.chord(Keys.ENTER))
 
-WebUI.waitForPageLoad(10)
+WebUI.waitForPageLoad(30)
 
 WebUI.scrollToPosition(0, 500)
 
@@ -35,7 +33,7 @@ WebUI.setText(findTestObject('input_price_max'), '30000000')
 
 WebUI.sendKeys(findTestObject('input_price_max'), Keys.chord(Keys.ENTER))
 
-WebUI.waitForPageLoad(10)
+WebUI.waitForPageLoad(30)
 
 WebUI.click(findTestObject('sort_paling_sesuai'))
 
@@ -51,7 +49,7 @@ WebUI.verifyElementPresent(findTestObject('page_two'), 10)
 
 WebUI.click(findTestObject('page_two'))
 
-WebUI.waitForPageLoad(10)
+WebUI.waitForPageLoad(30)
 
 List<WebElement> productPageTwo = WebUI.findWebElements(findTestObject('product_item'), 5)
 
@@ -63,10 +61,11 @@ WebUI.verifyElementPresent(findTestObject('page_three'), 10)
 
 WebUI.click(findTestObject('page_three'))
 
-WebUI.waitForPageLoad(10)
+WebUI.waitForPageLoad(30)
 
 List<WebElement> productPageThree = WebUI.findWebElements(findTestObject('product_item'), 5)
 
 CustomKeywords.'hubblePackage.WriteToExcel.storeData'(productPageThree, 'Page 3')
 
 WebUI.closeBrowser()
+
