6c104cd75989875d4c198cf23dc45986a995e3d7
sam_priskwila <sam.pris77@gmail.com>
2023-05-17 21:38:57 +0700

    add test cases, object repository, custom keywords, excel output file

---
+++ Object Repository/sort_harga_terendah.rs
@@ -0,0 +1,110 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>sort_harga_terendah</name>
+   <tag></tag>
+   <elementGuidId>24155811-9e40-4295-94c4-972ee55235ac</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='Terjual 40+'])[4]/following::button[5]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value>//button[@data-item-text = 'Harga Terendah' and (text() = 'Harga Terendah' or . = 'Harga Terendah')]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>button</value>
+      <webElementGuid>c36afce2-4057-41f9-a5d2-317a35801c8d</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>css-nzq9o0</value>
+      <webElementGuid>f0c92c29-c9e6-4745-b29e-0a8f94880ad8</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>data-unf</name>
+      <type>Main</type>
+      <value>select-menu-item-btn</value>
+      <webElementGuid>260a93fd-d166-42b2-88ab-2dc935ad1f8e</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>data-has-subitem</name>
+      <type>Main</type>
+      <value>false</value>
+      <webElementGuid>bbb3b513-4d99-4393-90cb-fe716705b17a</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>data-item-text</name>
+      <type>Main</type>
+      <value>Harga Terendah</value>
+      <webElementGuid>92863c4c-fc7e-4f12-8bae-373a0da7d7a8</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Harga Terendah</value>
+      <webElementGuid>358dcaff-a5ea-4ea8-a0d1-2e0cb2f30a43</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/div[7]/div[@class=&quot;css-aeiuzo e17suhym1&quot;]/div[@class=&quot;css-19os3da e83okfj4&quot;]/ul[@class=&quot;css-rxgulu&quot;]/li[@class=&quot;css-d2qf6r e83okfj5&quot;]/button[@class=&quot;css-nzq9o0&quot;]</value>
+      <webElementGuid>529cd01c-a2a1-42b0-82a1-874c439a151e</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Terjual 40+'])[4]/following::button[5]</value>
+      <webElementGuid>b53b47da-cb35-4c38-9be2-cb408214b97e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='iShop Here'])[1]/following::button[5]</value>
+      <webElementGuid>a1550538-06de-4b0d-ab24-03157fca9b72</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//li[5]/button</value>
+      <webElementGuid>a05ba4f3-56e3-42ae-963d-b10c96a9dae7</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//button[(text() = 'Harga Terendah' or . = 'Harga Terendah')]</value>
+      <webElementGuid>bdfe69b1-594b-42d6-8741-a57ace8a63b4</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
