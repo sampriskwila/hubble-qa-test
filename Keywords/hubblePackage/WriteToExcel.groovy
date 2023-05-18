71c945e7ddecd15db4dc4dcc0188f2b0a48c0d2f
sam_priskwila <sam.pris77@gmail.com>
2023-05-18 18:01:29 +0700

    extract method in WriteToExcel keywords

--- Keywords/hubblePackage/WriteToExcel.groovy
+++ Keywords/hubblePackage/WriteToExcel.groovy
@@ -14,20 +14,28 @@ public class WriteToExcel {
 	public void storeData(List<WebElement> elements, String sheetName) {
 		XSSFWorkbook workbook = new XSSFWorkbook()
 		XSSFSheet sheet = workbook.createSheet(sheetName)
-
+		
 		for (int i = 0; i < elements.size(); i++) {
 			Row row = sheet.createRow(i)
 			Cell cell = row.createCell(0)
 			cell.setCellValue(elements.get(i).getText())
 		}
-
+		
 		String filePath = RunConfiguration.getProjectDir() + "/Excel Output File/" + sheetName + ".xlsx"
+		File file = createFolder(filePath)
+		processOutputFile(workbook, file)
+	}
+	
+	private File createFolder(String filePath) {
+		File file = new File(filePath)
+		file.getParentFile().mkdirs()
+		file.createNewFile()
 		
-		File file = new File(filePath);
-		file.getParentFile().mkdirs();
-		file.createNewFile();
-		
-		FileOutputStream fileOutput = new FileOutputStream(file, false)
+		return file
+	}
+	
+	private void processOutputFile(XSSFWorkbook workbook, File file) {
+		FileOutputStream fileOutput = new FileOutputStream(file)
 		workbook.write(fileOutput)
 		fileOutput.close()
 	}
